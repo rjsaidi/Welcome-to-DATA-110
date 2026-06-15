ddc041b6c5fbe656f239249a6953c08fca444e85
Rachel Saidi <33728906+rjsaidi@users.noreply.github.com>
2026-06-15 09:07:54 -0400

    Add files via upload

---
+++ cowsay library.R
@@ -0,0 +1,24 @@
+install.packages("cowsay")
+library(cowsay)
+
+say(what = "I love coding in R!", by = "cow")
+
+say(what = "I love coding in R!", by = "cat")
+say(what = "I love coding in R!", by = "yoda")
+say(what = "I love coding in R!", by = "pig")
+say(what = "I love coding in R!", by = "monkey")
+say(what = "I love coding in R!", by = "stegosaurus")
+say(what = "I love coding in R!", by = "chicken")
+say(what = "I love coding in R!", by = "owl")
+say(what = "I love coding in R!", by = "bigcat")
+
+
+`by` must be one of "alligator", "ant", "bat", "bat2", "beavis",
+"behindcat", "bigcat", "blowfish", "buffalo", "cat", "chicken", "chuck",
+"clippy", "cow", "cow_borg", "cow_dead", "cow_greedy", "cow_sleepy",
+"cow_tired", "cow_wired", "cow_young", "daemon", "dragon", "duck",
+"duckling", "egret", "endlesshorse", "facecat", "frog", "ghost",
+"goldfish", "hypnotoad", "monkey", "owl", "pig", "poop", "pumpkin",
+"rabbit", "shark", "smallcat", "snowman", "spider", "squirrel",
+"squirrel2", "stegosaurus", "trilobite", "turkey", "whale", "wolf",
+"yoda", "rms", or "random", not "dog".
---
+++ shiny examples.R
@@ -0,0 +1,12 @@
+library(shiny)
+runExample("01_hello", display.mode = "showcase")      # a histogram
+runExample("02_text")       # tables and data frames
+runExample("03_reactivity") # a reactive expression
+runExample("04_mpg")        # global variables
+runExample("05_sliders")    # slider bars
+runExample("06_tabsets")    # tabbed panels
+runExample("07_widgets")    # help text and submit buttons
+runExample("08_html")       # Shiny app built from HTML
+runExample("09_upload")     # file upload wizard
+runExample("10_download")   # file download wizard
+runExample("11_timer")      # an automated timer
